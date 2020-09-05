c95ee1439bb21da66732595f1a1264b7cacdecf7
nickkutsevolov <idontwanttousemyactualemail@whatever.com>
2020-09-05 20:34:35 +0300

    1

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -36,20 +36,26 @@ function Pagination({ rows, getRange, getFilter }) {
 
     return (
         <div className='flex justify-between items-baseline'>
-            <div className="inline-block px-2 h-8 mx-1 leading-7 rounded-full text-center">
-                <span>Filter by name:</span>
-                <input className="bg-transparent border-b border-black" onChange={e => {
+            <div class="relative">
+				<input type="search" class="w-full pl-10 pr-4 py-2 rounded-lg shadow focus:outline-none focus:shadow-outline text-gray-600 font-medium" placeholder="Search..." onChange={e => {
                     if (e.target.value.match(/[A-Za-z]/g)) {
                         getFilter(e.target.value.match(/[A-Za-z]/g).join('').toLowerCase());
                         setCurrentPage(1);
-                    } else getFilter(/[A-Za-z]/g)}} />
-            </div>
+                    } else getFilter(/[A-Za-z]/g)}}/>
+				<div class="absolute top-0 left-0 inline-flex items-center p-2">
+					<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-gray-400" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+						<rect x="0" y="0" width="24" height="24" stroke="none"></rect>
+						<circle cx="10" cy="10" r="7"></circle>
+						<line x1="21" y1="21" x2="15" y2="15"></line>
+					</svg>
+				</div>
+			</div>
             <div className='flex items-baseline'>
                 <div className="inline-flex mx-auto justify-center py-1 select-none text-center leading-7" onClick={e => handler(e.target.innerText)}>
                     <div className={`w-8 h-8 mx-1 rounded-full text-center ${currentPage === 1 ? 'hidden' : 'cursor-pointer hover:bg-gray-300'}`}>&lt;</div>
                     {controls.map((el, index) =>
                         <div key={index ^ 2}
-                            className={`w-8 h-8 mx-1 rounded-full cursor-pointer leading-8 hover:bg-gray-300 ${el === currentPage ? 'bg-gray-600 text-white' : ''}`}
+                            className={`w-8 h-8 mx-1 rounded-full leading-8 ${el === currentPage ? 'bg-gray-600 text-white' : el === '...' ? '' : 'hover:bg-gray-300 cursor-pointer'}`}
                             onClick={e => handler(e.target.innerText)}>{el}</div>
                     )}
                     <div className={`w-8 h-8 mx-1 rounded-full text-center ${currentPage === pages ? 'hidden' : 'cursor-pointer hover:bg-gray-300'}`}>&gt;</div>
