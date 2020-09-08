6b5e87abf7d413fe89503451962d7f4e2a993e1c
nickkutsevolov <idontwanttousemyactualemail@whatever.com>
2020-09-09 00:38:28 +0300

    1

--- src/App.js
+++ src/App.js
@@ -6,20 +6,23 @@ import TendencyChart from './components/TendencyChart';
 function App() {
   return (
     <>
-      <div className="w-full h-screen bg-hero flex flex-row items-center">
-        <div className="w-1/2 flex flex-column text-2xl">
-          <div className="w-48 cursor-pointer">What's COVID-19</div>
-          <div className="w-48 cursor-pointer">Total Chart</div>
-          <div className="w-48 cursor-pointer">Tendency Chart</div>
-          <div className="w-48 cursor-pointer">Table</div>
-        </div>
-        <div className="w-1/2 font-normal text-right mr-16">
-          <h1 className="text-5xl">COVID-19 PANDEMIC</h1>
-          <h3 className="text-4xl">Stay Home. Stay Safe.</h3>
+      <div className="w-full h-screen flex items-center">
+        <div className="w-3/5 h-full bg-hero" />
+        <div className="w-2/5 h-full bg-white grid p-12">
+          <div className="w-full flex text-lg justify-around">
+            <div className="cursor-pointer">COVID-19</div>
+            <div className="cursor-pointer">Total Chart</div>
+            <div className="cursor-pointer">Tendency Chart</div>
+            <div className="cursor-pointer">Table</div>
+          </div>
+          <div className="w-full font-normal">
+            <h1 className="text-5xl">COVID-19 PANDEMIC</h1>
+            <h3 className="text-4xl">Stay Home. Stay Safe.</h3>
+          </div>
         </div>
       </div>
       <div className="w-1/2 m-auto my-16">
-        <h3 className="text-4xl text-center text-red-400">What's COVID-19</h3>
+        <h3 className="text-4xl text-red-400">What's COVID-19</h3>
         <p className="text-l my-2">Coronavirus (COVID-19) is an illness caused by a virus that can spread from person to person. The virus that causes COVID-19 is a new coronavirus that has spread throughout the world. COVID-19 symptoms can range from mild (or no symptoms) to severe illness.</p>
         <p className="text-l my-2">You can become infected by coming into close contact (about 6 feet or two arm lengths) with a person who has COVID-19. COVID-19 is primarily spread from person to person. You can become infected from respiratory droplets when an infected person coughs, sneezes, or talks. You may also be able to get it by touching a surface or object that has the virus on it, and then by touching your mouth, nose, or eyes.</p>
         <p className="text-l my-2">There is currently no vaccine to protect against COVID-19. The best way to protect yourself is to avoid being exposed to the virus that causes COVID-19. Stay home as much as possible and avoid close contact with others. Wear a mask that covers your nose and mouth in public settings. Clean and disinfect frequently touched surfaces. Wash your hands often with soap and water for at least 20 seconds, or use an alcoholbased hand sanitizer that contains at least 60% alcohol.</p>
@@ -29,7 +32,7 @@ function App() {
       </div>
       <div className="flex m-16">
         <div className="w-1/3 p-12">
-          <h3 className="text-4xl text-center text-red-400">Total Chart</h3>
+          <h3 className="text-4xl text-red-400">Total Chart</h3>
           <p className="text-l">Exercitation minim ex occaecat pariatur ex sit consectetur Lorem consequat pariatur ipsum nostrud. Duis sunt cillum in dolor anim quis anim deserunt labore laborum aute ea quis. In id nisi quis labore pariatur aliqua cillum voluptate qui eiusmod fugiat est. Nisi laborum incididunt excepteur eiusmod ipsum elit.</p>
         </div>
         <TotalChart />
@@ -37,7 +40,7 @@ function App() {
       <div className="flex m-16">
         <TendencyChart />
         <div className="w-1/3 p-12">
-          <h3 className="text-4xl text-center text-red-400">Tendency Chart</h3>
+          <h3 className="text-4xl text-red-400">Tendency Chart</h3>
           <p className="text-l">Exercitation minim ex occaecat pariatur ex sit consectetur Lorem consequat pariatur ipsum nostrud. Duis sunt cillum in dolor anim quis anim deserunt labore laborum aute ea quis. In id nisi quis labore pariatur aliqua cillum voluptate qui eiusmod fugiat est. Nisi laborum incididunt excepteur eiusmod ipsum elit.</p>
         </div>
       </div>
--- src/components/Table.js
+++ src/components/Table.js
@@ -57,18 +57,18 @@ function Table() {
     }
 
     return (
-        <div className="container mx-auto max-w-6xl">
+        <div className="container m-16 w-auto">
             <Pagination rows={data.length}  getRange={range => setRange(range)} getFilter={filterHandler}/>
             <table className="w-full table-auto border rounded-lg overflow-hidden bg-white my-2">
                 <thead>
                     <tr className="text-gray-600 cursor-pointer" onClick={sortHandler}>
-                        <th className="font-normal text-right px-4 py-2 cursor-default w-16">#</th>
-                        <th className="font-normal px-4 py-2 text-left">Country ↓</th>
-                        <th className="font-normal text-right px-4 py-2">Confirmed</th>
-                        <th className="font-normal text-right px-4 py-2">Deaths</th>
-                        <th className="font-normal text-right px-4 py-2">Recovered</th>
-                        <th className="font-normal text-right px-4 py-2">Lethality</th>
-                        <th className="font-normal text-right px-4 py-2">InfectionRate</th>
+                        <th className="font-normal text-right py-2 cursor-default w-16">#</th>
+                        <th className="font-normal py-2 text-left">Country ↓</th>
+                        <th className="font-normal text-right py-2">Confirmed</th>
+                        <th className="font-normal text-right py-2">Deaths</th>
+                        <th className="font-normal text-right py-2">Recovered</th>
+                        <th className="font-normal text-right py-2">Lethality</th>
+                        <th className="font-normal text-right py-2">InfectionRate</th>
                     </tr>
                 </thead>
                 <tbody>
